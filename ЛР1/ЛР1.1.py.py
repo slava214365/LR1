1f0b0f1a50ede1b976a548daaff8a2e0d1829409
Найденов Вячеслав <147939307+slava214365@users.noreply.github.com>
2023-10-15 10:45:58 +0300

    Update ЛР1.1.py.py

--- ЛР1/ЛР1.1.py.py
+++ ЛР1/ЛР1.1.py.py
@@ -1,10 +1,10 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-numbers[4] = 0  ## Не знаю, как можно заменить, не считая руками
-unique_numbers = set(numbers)
-sum_of_numbers = sum(unique_numbers)
-count_of_numbers = len(unique_numbers)
+missing_index = 4
+numbers[missing_index] = 0
+sum_of_numbers = sum(numbers)
+count_of_numbers = len(numbers)
 new_number = sum_of_numbers / count_of_numbers
-numbers[4] = new_number
+numbers[missing_index] = new_number
 
 print("Измененный список:", numbers)
